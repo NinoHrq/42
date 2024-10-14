0015e07a269e7529d81d4788bed40410c49cfbca
NinoHrq <nour.harraqi@gmail.com>
2024-10-14 16:55:49 +0200

    Add files via upload

--- minitalk/minitalk.h
+++ minitalk/minitalk.h
@@ -6,18 +6,23 @@
 /*   By: nharraqi <nharraqi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 14:30:39 by nharraqi          #+#    #+#             */
-/*   Updated: 2024/10/07 19:02:18 by nharraqi         ###   ########.fr       */
+/*   Updated: 2024/10/14 16:53:46 by nharraqi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINITALK_H
 # define MINITALK_H
 
+# include "libft.h"
 # include <signal.h>
 # include <stdlib.h>
 # include <unistd.h>
-# include "libft.h"
+# define SIZE 150000
 
+// server.c
 void	handle_signal(int sig);
-void	send_char(pid_t pid, char c);
+
+// client.c
+void	send_char(int pid, char c);
+
 #endif
