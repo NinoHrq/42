dcb1b0068c4c64ab7d149e5cf319066a4f7c4458
Nour Harraqi <nharraqi@made-f0Ar6s1.clusters.42paris.fr>
2024-10-26 22:15:26 +0200

    xxxx

--- Rev Exam02/lvl1/rev_print.c
+++ Rev Exam02/lvl1/rev_print.c
@@ -2,10 +2,10 @@
 
 int	main(int ac, char **av)
 {
-	if (ac == 2)
-	{
 		int i;
 
+	if (ac == 2)
+	{
 		i = 0;
 		while (av[1][i])
 			i++;
@@ -13,4 +13,18 @@ int	main(int ac, char **av)
 			write(1, &av[1][i], 1);
 	}
 	write(1, "\n", 1);
+}
+
+#include <unistd.h>
+
+char	*rev_print(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+		i++;
+	while (--i >= 0)
+		write(1, &str[i], 1);
+	write(1, "\n", 1);
+	return (str);
 }
