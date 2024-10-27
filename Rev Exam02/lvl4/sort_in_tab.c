f418114e04c8043e0401d3c30ad431f1a8d1598f
Nour Harraqi <nharraqi@made-f0Dr11s18.clusters.42paris.fr>
2024-10-27 17:10:40 +0100

    xxxx

--- Rev Exam02/lvl4/sort_in_tab.c
+++ Rev Exam02/lvl4/sort_in_tab.c
@@ -1,7 +1,7 @@
 void	sort_in_tab(int *tab, unsigned int len)
 {
-	unsigned int i;
-	int tmp;
+	unsigned int	i;
+	int				tmp;
 
 	i = 0;
 	while (i < (len - 1))
@@ -13,6 +13,30 @@ void	sort_in_tab(int *tab, unsigned int len)
 			tab[i + 1] = tmp;
 			i = 0;
 		}
-		i++;
+		else
+			i++;
 	}
+}
+
+#include <stdio.h>
+
+void	sort_in_tab(int *tab, unsigned int len);
+
+int	main(void)
+{
+	int tab[] = {5, 2, 9, 1, 5, 6, 1, 2, 5, -1, 4};                 
+		// tableau d'exemple avec des valeurs non triées
+	unsigned int len = sizeof(tab) / sizeof(tab[0]);
+		// calcul de la longueur du tableau
+	printf("Tableau avant tri :\n");
+	for (unsigned int i = 0; i < len; i++)
+		printf("%d ", tab[i]);
+	printf("\n");
+	// Appel de la fonction pour trier le tableau
+	sort_in_tab(tab, len);
+	printf("Tableau après tri :\n");
+	for (unsigned int i = 0; i < len; i++)
+		printf("%d ", tab[i]);
+	printf("\n");
+	return (0);
 }
