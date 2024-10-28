c038e49bbb329433af3be999e3213c6924c51127
Nour Harraqi <nharraqi@made-f0Dr10s16.clusters.42paris.fr>
2024-10-28 15:26:02 +0100

    xxxx

---
+++ Rev Exam02/lvl4/flood_fill/flood_fill.h
@@ -0,0 +1,14 @@
+#ifndef FLOOD_FILL_H
+# define FLOOD_FILL_H
+
+typedef struct s_point
+{
+	int	x;
+	int	y;
+}		t_point;
+/*
+void	fill(char **tab, t_point size, t_point cur, char to_fill);
+void	flood_fill(char **tab, t_point size, t_point begin);
+char	**make_area(char **zone, t_point size);
+*/
+#endif
